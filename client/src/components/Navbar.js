fbf40123c639dae3d17112cd84ad1da4ee519b3a
adirn26 <adirn26@gmail.com>
2023-05-07 13:54:34 +0530

    Initial-commit

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+            <div className="container">
+                <a className="navbar-brand" href="#"><h3>Med<span class="text-danger-emphasis">Care</span></h3></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                    <button className="btn btn-outline-success" type="submit">Login</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
